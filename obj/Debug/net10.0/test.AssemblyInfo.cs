68c131deeec6ad5a240bf0fb4a3761d9b3314875
noahldeschenes <noahldeschenes@gmail.com>
2026-06-01 14:09:01 -0400

    Changes to be committed

--- obj/Debug/net10.0/test.AssemblyInfo.cs
+++ obj/Debug/net10.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df869980d1fb239475d300bd0c35b0c41036d723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb778cae832b31cb203dcef76a714c759808d98a")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
