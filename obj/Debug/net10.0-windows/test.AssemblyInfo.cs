39aef23c80d1fb05e1811d7d8f197f7760b51d7f
noahldeschenes <noahldeschenes@gmail.com>
2026-06-01 15:37:47 -0400

    Trivial

--- obj/Debug/net10.0-windows/test.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c131deeec6ad5a240bf0fb4a3761d9b3314875")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b17f2ab848f924ff87c02ec0345a01b593e9a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- test.cs
+++ test.cs
@@ -10,6 +10,11 @@ namespace Test
         {
             var server = new UsbFsm100ServerClass(Process.GetCurrentProcess().Handle);
             Console.WriteLine(server.ConnectionStatus);
+            while (true)
+            {
+                Console.WriteLine(server.CommandAndReceiveText("=FUNCSTAT"));
+                Thread.Sleep(3000);
+            }
         }
         
     }
