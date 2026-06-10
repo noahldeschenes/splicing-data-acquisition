38182fcafbd6152e5336ccecc5c480a500dfbd86
noahldeschenes <noahldeschenes@gmail.com>
2026-06-10 10:17:31 -0400

    trivial

--- RecordSplicingResults/Program.cs
+++ RecordSplicingResults/Program.cs
@@ -3,6 +3,7 @@
 
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using Utils;
 
 
 namespace RecordSplicingResults
@@ -11,21 +12,17 @@ namespace RecordSplicingResults
         Folder: date
         Files inside: JSON, images, settings, errors
 
-        maybe add original arc time/power and give a difference? ask mark
-
     */
 
     class Program
     {
 
-        static Dictionary<string, string> GetDictFromSplicerOutput(string splicerOutput)
+        static Dictionary<string, object> ParseSplicerOutput(string splicerOutput)
         {
             // <summary> Takes the output of the splicer and formats it into a dict, for eventual
             // conversion into a JSON. </summary>
 
-            // TODO: add conversions to ints/floats
-
-            Dictionary<string, string> pairs = new();
+            Dictionary<string, object> pairs = new();
 
             string pattern = @"(?<identifier>[^|=]+)=(?<result>[^|]*)";  // input form: IDENTIFIER1=RESULT1|IDENTIFIER2=RESULT2|...   
 
@@ -33,7 +30,16 @@ namespace RecordSplicingResults
             {
                 string id = match.Groups["identifier"].Value;
                 string result = match.Groups["result"].Value; 
-                pairs[id] = result;
+
+                
+                // result can be a string, an int, or a float, so we auto-parse it for the sake of convenience/flexibility
+                
+                if (!int.TryParse(result, out int convertedResult) && !float.TryParse(result, out float convertedResult))
+                {
+                    string convertedResult = result;
+                }
+
+                pairs[id] = convertedResult;
             }
 
             return pairs;
@@ -42,7 +48,15 @@ namespace RecordSplicingResults
 
         static string QuerySplicer(string queryType, string[] identifiers)
         {
-            return "";
+            
+            string input = queryType;
+            
+            foreach (string id in identifiers)
+            {
+                input+=$"|{id}";
+            }
+
+            SplicerUtils.splicer.CommandAndRecieveText(input);
         }
 
         
